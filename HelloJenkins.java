fda458c925ecb4d9a7ac56e27bea77df824a4b53
pallavi reddy <marikantipallavireddy@gmail.com>
2026-08-12 15:14:00 +0530

    Add files via upload

---
+++ HelloJenkins.java
@@ -0,0 +1,11 @@
+public class HelloJenkins {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("Hello Jenkins!");
+        System.out.println("Continuous Integration Demo");
+        System.out.println("Sum of " + a + " + " + b + " = " + sum);
+    }
+}
